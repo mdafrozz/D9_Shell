53509f1590d890c4a1db84f7332d03094beac283
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-06-07 14:26:07 +0530

    Refactor: Calculating Wages for a Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,9 +5,11 @@ echo "******** Welcome to Employee Wage Computation Program ************"
 WAGE_PER_HR=20
 FULL_DAY_WORKING_HR=8
 HALF_DAY_WORKING_HR=4
-
+MAX_WORKING_DAYS=20
 dailyWage=0
 
+for (( i=0;i<$MAX_WORKING_DAYS;i++ ))
+do
 attendance=$((RANDOM%3))
 case $attendance in
 	1)
@@ -27,3 +29,4 @@ case $attendance in
 		dailyWage=0
 		echo "Daily wage: $dailyWage"
 esac
+done
